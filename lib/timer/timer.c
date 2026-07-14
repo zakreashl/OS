edf1a7e30bdc250e0ddb3b4a450369c720062083
zakreashl <zakreashlaibah@gmail.com>
2026-07-14 14:11:58 -0500

    fixed timer callback bug
    end was not being moved after adding another callback

--- lib/timer/timer.c
+++ lib/timer/timer.c
@@ -5,11 +5,16 @@
 timer_callback timer_callbacks[];
 timer_callback* end = &timer_callbacks[0];
 int system_ticks = 0;
+int callback_count = 0;
 
 void H_timer() {
     system_ticks++;
+    if(callback_count == 0) {
+        outb(0x20, 0x20);
+        return;
+    }
 
-    for(int i = 0; i < CALLBACK_COUNT; i++) {
+    for(int i = 0; i < callback_count; i++) {
         timer_callback callback = timer_callbacks[i];
         if(system_ticks % callback.interval == 0) {
             callback.timer_callback_handler_ptr();
@@ -20,7 +25,9 @@ void H_timer() {
 }
 
 extern void add_timer_callback(void (*timer_callback_handler_function), int interval) {
-    if(&timer_callbacks[0] - end > CALLBACK_COUNT) return;
+    if(&timer_callbacks[0] - end > CALLBACK_MAX_COUNT) return;
     end->timer_callback_handler_ptr = timer_callback_handler_function;
     end->interval = interval;
+    end++;
+    callback_count++;
 }
--- lib/timer/timer.h
+++ lib/timer/timer.h
@@ -2,7 +2,7 @@
 #define TIMER_H
 
 #define TPS 100 // ticks per second
-#define CALLBACK_COUNT 50
+#define CALLBACK_MAX_COUNT 50
 
 typedef struct {
     void (*timer_callback_handler_ptr)();
@@ -11,12 +11,14 @@ typedef struct {
 
 
 // TO-DO: make this into a list v
-extern timer_callback timer_callbacks[CALLBACK_COUNT];
+extern timer_callback timer_callbacks[CALLBACK_MAX_COUNT];
 
 extern timer_callback* end;
 
 // track number of ticks
 extern int system_ticks;
+
+extern int callback_count;
 
 void H_timer();
 extern void H_timer_stub();
